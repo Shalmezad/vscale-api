af481ea21e710b2a3d368abdbbdbd7787710d3c3
Richard Wardin <richardw@hesonline.com>
2016-10-17 08:46:23 -0400

    More tests (domain_records and domain_tags)

--- spec/api/domains_spec.rb
+++ spec/api/domains_spec.rb
@@ -68,6 +68,78 @@ describe Vscale::Domains do
       end
     end
 
-  end
-  
+    describe "#update_domain_record" do
+      it "calls #put with the correct url and params" do
+        domain_id = 90210
+        record_id = 90211
+        params = {:hello => :world}
+        @api.should_receive(:put).with("domains/90210/records/90211", params)
+        @api.update_domain_record domain_id, record_id, params
+      end
+    end
+
+    describe "#remove_domain_record" do
+      it "calls #delete with the correct url" do
+        domain_id = 90210
+        record_id = 90211
+        @api.should_receive(:delete).with("domains/90210/records/90211")
+        @api.remove_domain_record domain_id, record_id
+      end
+    end
+
+    describe "#domain_record" do
+      it "calls #get with the correct url" do
+        domain_id = 90210
+        record_id = 90211
+        @api.should_receive(:get).with("domains/90210/records/90211")
+        @api.domain_record domain_id, record_id
+      end
+    end
+
+  end #describe "Domain Records" do
+
+  describe "Domain Tags" do
+
+    describe "#add_domains_tags" do
+      it "calls #post with the correct url and params" do
+        params = {:hello => :world}
+        @api.should_receive(:post).with("domains/tags/", params)
+        @api.add_domains_tags params
+      end
+    end
+
+    describe "#domains_tags" do
+      it "calls #get with the correct url" do
+        @api.should_receive(:get).with("domains/tags/")
+        @api.domains_tags
+      end
+    end
+
+    describe "#domains_tag_id" do
+      it "calls #get with the correct url" do
+        tag_id = 90210
+        @api.should_receive(:get).with("domains/tags/90210")
+        @api.domains_tag_id tag_id
+      end
+    end
+
+    describe "#update_domains_tag" do
+      it "calls #put with the correct url and params" do
+        tag_id = 90210
+        params = {:hello => :world}
+        @api.should_receive(:put).with("domains/tags/90210", params)
+        @api.update_domains_tag tag_id, params
+      end
+    end
+
+    describe "#remove_domains_tag" do
+      it "calls #delete with the correct url" do
+        tag_id = 90210
+        @api.should_receive(:delete).with("domains/tags/90210")
+        @api.remove_domains_tag tag_id
+      end
+    end
+
+  end #describe "Domain Tags" do
+
 end
